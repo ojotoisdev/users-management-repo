2adf1a13871fc42688b444e3b3d5cce2eaca1195
ojotoisdev <129745992+ojotoisdev@users.noreply.github.com>
2025-03-22 22:25:39 +0100

    update features

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store/userSlice";
 import { RootState, AppDispatch } from "../store/store";
 import UserCard from "./UserCard";
+import UserForm from "./UserForm"; // Import the UserForm component
 
 const UserList: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { users, loading, error } = useSelector((state: RootState) => state.users);
+
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -17,6 +20,30 @@ const UserList: React.FC = () => {
 
   return (
     <div className="user-list">
+      {/* Add User Button */}
+      <button 
+        style={{
+          marginBottom: "10px",
+          padding: "10px",
+          backgroundColor: "green",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+        onClick={() => setShowForm(true)}
+      >
+        + Add User
+      </button>
+
+      {/* Show User Form when button is clicked */}
+      {showForm && (
+        <UserForm 
+          onClose={() => setShowForm(false)} 
+        />
+      )}
+
+      {/* User Cards */}
       {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
